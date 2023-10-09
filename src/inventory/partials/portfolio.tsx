5b62a4f56b2edaef7a1c62c6521d383c5d833c84
Daniel Toth <daniel.toth@adidas.com>
2023-10-09 14:42:33 +0200

    Adds key

--- src/inventory/partials/portfolio.tsx
+++ src/inventory/partials/portfolio.tsx
@@ -6,12 +6,21 @@ import { Dispatch } from "redux";
 import { SongStoreAction } from "../../shared/stores/songsStore";
 
 export default function Portfolio() {
-  const shares = useSelector(selectShares)
-  const dispatch = useDispatch<Dispatch<SongStoreAction>>()
+  const shares = useSelector(selectShares);
+  const dispatch = useDispatch<Dispatch<SongStoreAction>>();
   return (
     <Tile className="flex-grow">
       <TileTitle>Portfolio</TileTitle>
-      {shares.map(share => <button onClick={() => dispatch({ type: 'sell', payload: { shareId: share.shareId} })}>{share.songId}: {share.name}</button>)}
+      {shares.map((share) => (
+        <button
+          key={share.shareId}
+          onClick={() =>
+            dispatch({ type: "sell", payload: { shareId: share.shareId } })
+          }
+        >
+          {share.songId}: {share.name}
+        </button>
+      ))}
     </Tile>
   );
 }
