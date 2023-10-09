5a4e9f2a28764e830292401cc4503fe4700e90b1
Daniel Toth <daniel.toth@adidas.com>
2023-10-09 11:24:01 +0200

    refactoring

--- src/marketplace/partials/markteplace.tsx
+++ src/marketplace/partials/markteplace.tsx
@@ -5,12 +5,17 @@ import { useSongsList } from "../clients/songs";
 
 export default function Marketplace() {
   const authToken = useContext<string>(AuthContext);
-  const { songs, error } = useSongsList(authToken);
-  return (
-    <div className="px-2 w-full h-full overflow-x-scroll">
-      {error && <p>Oh no! You ddos'd spotify too hard :/</p>}
-      {songs && songs.length === 0 && <p>No songs loaded</p>}
-      {songs &&
+  const { songs, status } = useSongsList(authToken);
+
+  const Inner = () => {
+    if (status === "error") return;
+    <p>Oh no! We seemed to have encountered an error</p>;
+
+    if (status === "loading") return <p>Loading...</p>;
+    if (status === "success")
+      return songs.length === 0 ? (
+        <p>No songs found</p>
+      ) : (
         songs.map((song) => (
           <SongSummary
             imageUrl={song.album.images[0]?.url ?? ""}
@@ -20,7 +25,12 @@ export default function Marketplace() {
             year={song.album.release_date}
             popularity={song.popularity}
           ></SongSummary>
-        ))}
+        ))
+      );
+  };
+  return (
+    <div className="px-2 w-full h-full overflow-x-scroll">
+      <Inner />
     </div>
   );
 }
