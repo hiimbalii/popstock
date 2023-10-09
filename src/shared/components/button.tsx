bb7e121f03c3f8b3eaf2828556073a5882e23b63
Daniel Toth <daniel.toth@adidas.com>
2023-10-09 11:51:19 +0200

    fixes props for button

--- src/shared/components/button.tsx
+++ src/shared/components/button.tsx
@@ -1,4 +1,4 @@
-export interface ButtonProps {
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
   color?: "primary" | "secondary";
   children: React.ReactNode;
 }
