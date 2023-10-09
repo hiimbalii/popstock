25613849a29f63d1ec3478cf5a7235ca85e6ee53
Daniel Toth <daniel.toth@adidas.com>
2023-10-09 10:27:21 +0200

    Adds minor styling upgrades

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,18 @@ import "./App.css";
 import Wallet from "./partial/wallet";
 import Portfolio from "./partial/portfolio";
 import Marketplace from "./partial/markteplace";
-import getAuthToken from "./clients/auth_client";
+import getAuthToken from "./AppRoot/auth_client";
 import AuthProvider from "./providers/authProvider";
 
 function App() {
   return (
     <AuthProvider>
-      <div className="bg-gray-600 w-full min-h-screen flex px-20">
+      <div className="bg-gray-600 w-full h-screen flex px-20">
         <div className="w-96 m-4 flex flex-col gap-2">
           <Portfolio />
           <Wallet />
         </div>
-        <div className="flex-grow m-4">
+        <div className="flex-grow m-4 h-full">
           <Marketplace />
         </div>
       </div>
