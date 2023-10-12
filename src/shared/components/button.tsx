2673d295123a77f136d5bf4a573b75af9424b129
Daniel Toth <daniel.toth@adidas.com>
2023-10-12 15:52:33 +0200

    adds disabled state

--- src/shared/components/button.tsx
+++ src/shared/components/button.tsx
@@ -1,11 +1,12 @@
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   color?: "primary" | "secondary";
   children: React.ReactNode;
 }
 
 const colors = {
-  primary: "bg-green-500 hover:bg-green-600",
-  secondary: "bg-gray-600 hover:bg-gray-700",
+  primary: "bg-green-500 hover:bg-green-600 disabled:bg-green-300 ",
+  secondary: "bg-gray-600 hover:bg-gray-700 disabled:bg-gray-300",
 };
 export default function Button({
   color = "secondary",
@@ -14,7 +15,7 @@ export default function Button({
 }: ButtonProps) {
   return (
     <button
-      className={`rounded-full py-1 px-3 my-1 ${colors[color]}`}
+      className={`rounded-full py-1 px-3 my-1 disabled:cursor-not-allowed ${colors[color]}`}
       {...props}
     >
       {children}
