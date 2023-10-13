47bd94ebb9aeda8c8b00e4b16421746f6804afef
Daniel Toth <daniel.toth@adidas.com>
2023-10-13 10:22:20 +0200

    checks ids array to make sure the request is valid

--- src/inventory/clients/get_track_prices.ts
+++ src/inventory/clients/get_track_prices.ts
@@ -2,6 +2,7 @@ const getTrackPrices = (
   auth_token: string,
   ids: string[]
 ): Promise<{ [key: string]: number }> => {
+  if (!ids || !ids.length) Promise.reject()
   const urlIds = encodeURIComponent(ids.join(","));
   return fetch(`https://api.spotify.com/v1/tracks?ids=${urlIds}`, {
     headers: { Authorization: `Bearer ${auth_token}` },
