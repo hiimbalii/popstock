88af28153eaa63fe0131145e82d68e717b78aeb2
Daniel Toth <tcrbt1@gmail.com>
2023-10-27 14:25:54 +0200

    Splits coreTypes.ts into their own type files

---
+++ src/common/types/share.ts
@@ -0,0 +1,8 @@
+import { TrackData } from "./track";
+
+export interface Share {
+  shareId: string;
+  quantity: number;
+  buyPrice: number;
+  trackData: TrackData;
+}
--- src/common/types/track.ts
+++ src/common/types/track.ts
@@ -1,9 +1,4 @@
-export interface Share {
-  shareId: string;
-  quantity: number;
-  buyPrice: number;
-  trackData: TrackData;
-}
+
 export interface TrackData {
   id: string;
   albumCoverUrl: string;
