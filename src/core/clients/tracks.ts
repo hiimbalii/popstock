a67c8c986eaf17d6f8dfa02e19da12274bb6f710
Daniel Toth <tcrbt1@gmail.com>
2023-10-27 15:57:20 +0200

    Simplifies client and makes signature more consistent

--- src/core/clients/tracks.ts
+++ src/core/clients/tracks.ts
@@ -1,23 +1,21 @@
 import {
-  selectLoadingState,
-  selectSearchTerm,
-  selectTracks,
-} from '../../common/selectors/selectors';
-import {TracksAction, fetchTracks} from '../actions/tracksActions';
-import {AppState} from '../store/store';
-import {useEffect, useRef} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {ThunkDispatch} from 'redux-thunk';
+  RecommendationsResponse,
+  TrackData,
+  mapRecommendationResponse,
+} from '../../common/types/track';
 
 const getTracks = (
   auth_token: string,
   searchString: string | null,
-): Promise<RecommendationsResponse> => {
+): Promise<TrackData[]> => {
   if (!searchString)
     return fetch(
       'https://api.spotify.com/v1/recommendations?seed_genres=alternative',
       {headers: {Authorization: `Bearer ${auth_token}`}},
-    ).then(res => res.json() as unknown as RecommendationsResponse);
+    )
+      .then(res => res.json() as unknown as RecommendationsResponse)
+      .then(mapRecommendationResponse);
 
   throw new Error('search not yet implemented');
 };
+export default getTracks;
