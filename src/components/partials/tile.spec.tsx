675520d75aaf53d86a1cd60f0a5d7429973cc91b
Daniel Toth <tcrbt1@gmail.com>
2023-10-31 16:20:07 +0100

    Adds tile spec

---
+++ src/components/partials/tile.spec.tsx
@@ -0,0 +1,50 @@
+import Tile, {TileProps, TileTitle} from './tile';
+import {render, screen} from '@testing-library/react';
+
+/// Criterias:
+/// * Render child
+/// * Render with default classes by default
+/// * Forward classes to container
+describe('<Tile />', () => {
+  const defaultProps: TileProps = {
+    children: (
+      <>
+        <p>Child 1</p>
+        <p>Child 2</p>
+      </>
+    ),
+  };
+  it('should render children', () => {
+    render(<Tile {...defaultProps} />);
+
+    expect(screen.getByText('Child 1')).toBeInTheDocument();
+    expect(screen.getByText('Child 2')).toBeInTheDocument();
+  });
+  it('should render with default classes without passing in anything', () => {
+    render(<Tile {...defaultProps} />);
+
+    expect(screen.getByTestId('tile')).toHaveClass('bg-gray-800');
+  });
+  it('should render with default and passed in classes', () => {
+    render(<Tile {...defaultProps} className='test-class' />);
+
+    expect(screen.getByTestId('tile')).toHaveClass('bg-gray-800');
+    expect(screen.getByTestId('tile')).toHaveClass('test-class');
+  });
+});
+
+/// Criterias
+/// * Render child (mostly sanity check)
+describe('<TileTitle />', () => {
+  it('should render children', () => {
+    render(
+      <TileTitle>
+        <span data-testid='child' />
+        Title
+      </TileTitle>,
+    );
+
+    expect(screen.getByTestId('child')).toBeInTheDocument();
+    expect(screen.getByText('Title')).toBeInTheDocument();
+  });
+});
