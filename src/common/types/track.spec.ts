790b12d4a352c7f1da50eb8ac6cefb9b11abbba5
Daniel Toth <tcrbt1@gmail.com>
2023-11-23 15:44:14 +0100

    Adds tests to mapRecommendationResponse

--- src/common/types/track.spec.ts
+++ src/common/types/track.spec.ts
@@ -1,30 +1,36 @@
-import {TrackData, TrackResponse, mapTrackResponse} from './track';
+import {
+  RecommendationsResponse,
+  TrackData,
+  TrackResponse,
+  mapRecommendationResponse,
+  mapTrackResponse,
+} from './track';
 
+const trackResponse: TrackResponse = {
+  album: {
+    name: 'album-name',
+    release_date: 'date',
+    images: [
+      {
+        url: 'url',
+      },
+    ],
+  },
+  id: 'id',
+  artists: [{name: 'artist-1'}],
+  name: 'track-name',
+  popularity: 4,
+};
+const track: TrackData = {
+  albumName: 'album-name',
+  id: 'id',
+  albumCoverUrl: 'url',
+  artist: 'artist-1',
+  title: 'track-name',
+  date: 'date',
+  popularity: 4,
+};
 describe('mapTrackResponse()', () => {
-  const trackResponse: TrackResponse = {
-    album: {
-      name: 'album-name',
-      release_date: 'date',
-      images: [
-        {
-          url: 'url',
-        },
-      ],
-    },
-    id: 'id',
-    artists: [{name: 'artist-1'}],
-    name: 'track-name',
-    popularity: 4,
-  };
-  const track: TrackData = {
-    albumName: 'album-name',
-    id: 'id',
-    albumCoverUrl: 'url',
-    artist: 'artist-1',
-    title: 'track-name',
-    date: 'date',
-    popularity: 4,
-  };
   it('should produce a Track from a TrackResponse with accurate values', () => {
     expect(mapTrackResponse(trackResponse)).toEqual(track);
   });
@@ -45,5 +51,17 @@ describe('mapTrackResponse()', () => {
 
     const result = mapTrackResponse(trackResponseWithArtists);
     expect(result.popularity).toBe(1);
+  });
+});
+
+describe('mapRecommendationResponse', () => {
+  const recommendationResponse: RecommendationsResponse = {
+    tracks: [trackResponse],
+  };
+  it('should produce an empty list from an empty response', () => {
+    expect(mapRecommendationResponse({tracks: []})).toEqual([]);
+  });
+  it('should produce a correct list of tracks from a response', () => {
+    expect(mapRecommendationResponse(recommendationResponse)).toEqual([track]);
   });
 });
