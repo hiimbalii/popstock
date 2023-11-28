7e0b1e42213eaad92351e3e53a437cfb616706d4
Daniel Toth <tcrbt1@gmail.com>
2023-11-28 11:30:06 +0100

    Set up msw

---
+++ jest.polyfills.js
@@ -0,0 +1,18 @@
+import {TextDecoder, TextEncoder} from 'node:util';
+
+Object.defineProperties(globalThis, {
+  TextDecoder: {value: TextDecoder},
+  TextEncoder: {value: TextEncoder},
+});
+
+import {Blob, File} from 'node:buffer';
+
+Object.defineProperties(globalThis, {
+  fetch: {value: fetch, writable: true},
+  Blob: {value: Blob},
+  File: {value: File},
+  Headers: {value: Headers},
+  FormData: {value: FormData},
+  Request: {value: Request},
+  Response: {value: Response},
+});
