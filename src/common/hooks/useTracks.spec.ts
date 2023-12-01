0d11465ede8ab03435ae711aef9993d7c3e5fb9d
Daniel Toth <tcrbt1@gmail.com>
2023-12-01 15:51:16 +0100

    Adds tests to useTracks hook

---
+++ src/common/hooks/useTracks.spec.ts
@@ -0,0 +1,86 @@
+import {useTrackList} from './useTracks';
+import {
+  createMockStore,
+  renderHookWithProvider,
+  trackMock,
+} from '../utils/test-utils';
+import * as trackActions from '../../core/actions/tracksActions';
+
+jest.mock('nanoid', () => ({
+  nanoid: () => 1,
+}));
+
+describe('useTrackList', () => {
+  beforeEach(() => jest.resetAllMocks());
+  it('should start fetching data if the current state is idle', () => {
+    const fetchSpy = jest.fn();
+    const actionSpy = jest
+      .spyOn(trackActions, 'fetchTracks')
+      .mockReturnValue(fetchSpy);
+    const store = createMockStore({
+      portfolio: {portfolio: [], wallet: 0},
+      tracks: {
+        loadingState: 'idle',
+        catalogue: {searchTerm: '', loadedTracks: []},
+      },
+    });
+    renderHookWithProvider(() => useTrackList('auth_token'), store);
+
+    expect(actionSpy).toBeCalled();
+    expect(fetchSpy).toBeCalled();
+  });
+  it('should return a load state and an empty list of tracks after fetching', () => {
+    const fetchSpy = jest.fn();
+    jest.spyOn(trackActions, 'fetchTracks').mockReturnValue(fetchSpy);
+
+    const store = createMockStore({
+      portfolio: {portfolio: [], wallet: 0},
+      tracks: {
+        loadingState: 'loading',
+        catalogue: {searchTerm: '', loadedTracks: []},
+      },
+    });
+    const {result} = renderHookWithProvider(
+      () => useTrackList('auth_token'),
+      store,
+    );
+
+    expect(result.current).toEqual({status: 'loading', tracks: []});
+  });
+  it('should return an error state and empty list of tracks if the request got rejected', () => {
+    const fetchSpy = jest.fn();
+    jest.spyOn(trackActions, 'fetchTracks').mockReturnValue(fetchSpy);
+
+    const store = createMockStore({
+      portfolio: {portfolio: [], wallet: 0},
+      tracks: {
+        loadingState: 'rejected',
+        catalogue: {searchTerm: '', loadedTracks: []},
+      },
+    });
+    const {result} = renderHookWithProvider(
+      () => useTrackList('auth_token'),
+      store,
+    );
+
+    expect(result.current).toEqual({status: 'rejected', tracks: []});
+  });
+  it('should return a success state and a list of fetched tracks if the request got a response', () => {
+    const fetchSpy = jest.fn();
+    jest.spyOn(trackActions, 'fetchTracks').mockReturnValue(fetchSpy);
+
+    const store = createMockStore({
+      portfolio: {portfolio: [], wallet: 0},
+      tracks: {
+        loadingState: 'success',
+        catalogue: {searchTerm: '', loadedTracks: [trackMock]},
+      },
+    });
+    const {result} = renderHookWithProvider(
+      () => useTrackList('auth_token'),
+      store,
+    );
+
+    expect(result.current).toEqual({status: 'success', tracks: [trackMock]});
+  });
+});
