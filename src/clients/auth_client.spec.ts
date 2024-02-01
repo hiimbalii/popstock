bbcf5f0eb955bda61c01942fa2e0f6140172e636
Daniel Toth <tcrbt1@gmail.com>
2024-02-01 14:14:21 +0100

    Removes old auth code

--- src/clients/auth_client.spec.ts
+++
@@ -1,4 +0,0 @@
-describe('getAuthToken', () => {
-  it.todo('should get auth token');
-  it.todo('should handle errors');
-});
--- src/common/hooks/useAuth.ts
+++
@@ -1,5 +0,0 @@
-import {AuthContext} from '../../core/providers/authProvider';
-import {useContext} from 'react';
-
-const useAuth = () => useContext(AuthContext);
-export default useAuth;
